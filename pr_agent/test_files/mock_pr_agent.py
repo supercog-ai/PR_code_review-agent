be3901900e8e2e396a4f434fe0c813e9633e65db
Brandon Li <brandonw.li1207@gmail.com>
2025-07-31 21:04:41 -0400

    fixes

--- pr_agent/test_files/mock_pr_agent.py
+++ pr_agent/test_files/mock_pr_agent.py
@@ -1,8 +1,8 @@
 # mock agent for testing github integration
 
+import json
 from agentic.common import Agent, AgentRunner 
 from agentic.models import GPT_4O_MINI # model
-from agentic.tools import GithubTool
 
 from dotenv import load_dotenv
 import openai
@@ -13,28 +13,38 @@ import os
 load_dotenv()  # This loads variables from .env into os.environ
 openai.api_key = os.getenv("OPENAI_API_KEY") # api key
 pr_id = os.getenv("PR_ID")
+repo_owner = os.getenv("REPO_OWNER")
+repo_name = os.getenv("REPO_NAME")
+gh_api = os.getenv("GITHUB_API_KEY")
 
 # Define the agent
 agent = Agent(
     name="Mock PR Summary Agent",
 
     # Agent instructions
-    instructions=f"""
+    instructions="""
     You are a helpful mock PR sumary agent to test github integration.
-    Create a helpful example of a PR summary.
-    Put a comment under the PR with the github tool. The issue number is {pr_id}.
+    Create a short, helpful example of a PR summary.
     """,
     
     model=GPT_4O_MINI, # model
-    tools=[GithubTool()],
-    memories=[]
 
 )
 
 # basic main function that allows us to run our agent locally in terminal
 if __name__ == "__main__":
-    output = agent.final_result(
-        "You were triggered by a PR opening/reopening. Follow your instructions."
+    output = agent.grab_final_result(
+        "You were triggered by a PR. Follow your instructions."
     )
 
-    print(output)
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues/{pr_id}/comments"
+
+    headers = {
+        "Authorization": f"token {gh_api}",
+    }
+
+    data = {
+        "body": output
+    }
+
+    print(requests.post(url=url,headers=headers,data=json.dumps(data)))
